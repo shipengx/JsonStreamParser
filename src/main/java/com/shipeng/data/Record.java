0b492f7e4885212e75866cb7e9bcec883289edf6
sxu <sxu@10.101.1.150>
2016-08-30 15:17:02 -0700

    added Record.java

---
+++ src/main/java/com/shipeng/data/Record.java
@@ -0,0 +1,87 @@
+package com.shipeng.data;
+
+
+public class Record {
+ 
+   private int adunitId;
+   private int slotVisibilityId;
+   private String domain;
+   private int mediaType;
+   private int categoryId;
+   private int riskId;
+   private int traqScoreId;
+   private int viewabilityScoreId;
+   private int ts;
+ 
+   public int getAdunitId() {
+      return this.adunitId;
+   }
+ 
+   public void setAdunitId(int value) {
+      this.adunitId = value;
+   }
+ 
+   public int getSlotVisibilityId() {
+      return this.slotVisibilityId;
+   }
+ 
+   public void setSlotVisibilityId(int value) {
+      this.slotVisibilityId = value;
+   }
+ 
+   public String getDomain() {
+      return this.domain;
+   }
+ 
+   public void setDomain(String value) {
+      this.domain = value;
+   }
+ 
+   public int getMediaType() {
+      return this.mediaType;
+   }
+ 
+   public void setMediaType(int value) {
+      this.mediaType = value;
+   }
+ 
+   public int getCategoryId() {
+      return this.categoryId;
+   }
+ 
+   public void setCategoryId(int value) {
+      this.categoryId = value;
+   }
+ 
+   public int getRiskId() {
+      return this.riskId;
+   }
+ 
+   public void setRiskId(int value) {
+      this.riskId = value;
+   }
+ 
+   public int getTraqScoreId() {
+      return this.traqScoreId;
+   }
+ 
+   public void setTraqScoreId(int value) {
+      this.traqScoreId = value;
+   }
+ 
+   public int getViewabilityScoreId() {
+      return this.viewabilityScoreId;
+   }
+ 
+   public void setViewabilityScoreId(int value) {
+      this.viewabilityScoreId = value;
+   }
+ 
+   public int getTs() {
+      return this.ts;
+   }
+ 
+   public void setTs(int value) {
+      this.ts = value;
+   }
+}
